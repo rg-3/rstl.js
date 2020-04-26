8c7a7471f3da740e4b3d26984ee84a6211a6441b
rg <1xab@protonmail.com>
2020-04-26 19:31:18 +0200

    v2.3.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,15 @@
 module.exports = (grunt) => {
   grunt.initConfig({
     browserify: grunt.file.readJSON("config/browserify.json"),
-    uglify: grunt.file.readJSON("config/uglify.json")
-  })
+    uglify: grunt.file.readJSON("config/uglify.json"),
+  });
+  grunt.config.package =  grunt.file.readJSON("package.json");
 
-  grunt.loadNpmTasks('grunt-browserify')
-  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask("es5",
                      "Create a minified, ES5-compatible version of src/browser.js that's suitable"+
                      " for use in the browser.",
-                     ["browserify", "uglify"])
-  grunt.registerTask("default", ["es5"])
-}
+                     ["browserify", "uglify"]);
+  grunt.registerTask("default", ["es5"]);
+};
